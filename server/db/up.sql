2556c3e77af13346c40c7e1301fa2fa032aa11ea
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2021-03-27 10:38:54 -0700

    starting deploy

--- server/db/up.sql
+++ server/db/up.sql
@@ -5,15 +5,15 @@ CREATE TABLE Users(
     photoUrl TEXT,
     username TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL,
-    TYPE TEXT NOT NULL,
+    type TEXT NOT NULL
 );
 
-CREATE TABLE OFFER(
+CREATE TABLE Offers(
     id UUID NOT NULL PRIMARY KEY UNIQUE DEFAULT uuid_generate_v4(),
     name TEXT NOT NULL,
     description TEXT NOT NULL,
     photoUrl TEXT,
-    usesLeft NUMBER DEFAULT 0,
+    usesLeft NUMERIC DEFAULT 0,
     frontlineWorkers BOOLEAN DEFAULT TRUE,
     vacinnated BOOLEAN DEFAULT TRUE,
     volunteers BOOLEAN DEFAULT TRUE
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ dotenv.config();
 import express from "express";
 import debug from "./routes/debug";
 import { Client } from "pg";
+import e from "express";
 
 const port = ((process.env.PORT as unknown) as number) ?? 8080;
 
@@ -12,6 +13,23 @@ let start = async () => {
   const client = new Client();
   await client.connect();
   app.use("/", debug());
+  app.post("/signup", (req, res) => {
+    client.query(
+      "INSERT INTO Users (username, password, type) VALUES ($1, $2, $3) RETURNING id",
+      [req.body.username, req.body.password, req.body.type],
+      (err, dbres) => {
+        if (err) {
+          res.status(501).send({
+            err: err,
+            message: "We had a database error 😭",
+          });
+        } else {
+          res.status(200).send(dbres.rows);
+        }
+      }
+    );
+  });
+
   app.listen(port, "0.0.0.0", () => {
     console.log("Running at http://0.0.0.0" + `:${port} ` + Date());
   });
