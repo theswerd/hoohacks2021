6015b5bb0fc7674c943a52c0542b5965dc35ace2
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2021-03-27 10:10:28 -0700

    ok

---
+++ server/db/up.sql
@@ -0,0 +1,16 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE Users(
+    id UUID NOT NULL PRIMARY KEY UNIQUE DEFAULT uuid_generate_v4(),
+    photoUrl TEXT,
+    username TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    TYPE TEXT NOT NULL,
+);
+
+CREATE TABLE OFFER(
+    id UUID NOT NULL PRIMARY KEY UNIQUE DEFAULT uuid_generate_v4(),
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    photoUrl TEXT,
+);
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,16 +2,19 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import debug from "./routes/debug";
-
-
-const app = express();
-app.use("/", debug());
-
-
+import { Client } from "pg";
 
 const port = ((process.env.PORT as unknown) as number) ?? 8080;
-app.listen(port, "0.0.0.0", () => {
-    console.log(
-      "Running at http://0.0.0.0" + `:${port} ` + Date()
-    );
+
+const app = express();
+
+let start = async () => {
+  const client = new Client();
+  await client.connect();
+  app.use("/", debug());
+  app.listen(port, "0.0.0.0", () => {
+    console.log("Running at http://0.0.0.0" + `:${port} ` + Date());
   });
+};
+
+start();
