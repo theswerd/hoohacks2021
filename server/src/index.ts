8a06dd7dfd2442687b8b6565773df9d186076757
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2021-03-27 11:21:35 -0700

    ah

--- server/src/index.ts
+++ server/src/index.ts
@@ -22,8 +22,8 @@ let start = async () => {
   app.use("/", debug());
   app.post("/signup", (req, res) => {
     client.query(
-      "INSERT INTO Users (username, password, type) VALUES ($1, $2, $3) RETURNING id",
-      [req.body.username, req.body.password, req.body.type],
+      "INSERT INTO Users (username, password, type, photoUrl) VALUES ($1, $2, $3, $4) RETURNING id",
+      [req.body.username, req.body.password, req.body.type, "https://media-exp1.licdn.com/dms/image/C5603AQHjTGI5E6C-jA/profile-displayphoto-shrink_400_400/0/1574749440663?e=1622073600&v=beta&t=7IbOHRZe5S6CGxCg5tpB7VXoi1lc9kyd39AhlTjoQzE"],
       (err, dbres) => {
         if (err) {
           res.status(501).send({
